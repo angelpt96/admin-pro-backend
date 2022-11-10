eb38e5f02cb069388ce5203b58199831337b92d8
Angel Parreño <angelpartoh@gmail.com>
2022-11-10 13:54:27 +0100

    Seccion 12

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { login } = require ('../controllers/auth');
+const { login, googleSignIn } = require ('../controllers/auth');
 const { validarCampos } = require ('../middlewares/validar-campos');
 
 const router = Router();
@@ -10,6 +10,11 @@ router.post( '/', [
     check('password', 'El password es obligatorio').not().isEmpty(),
     validarCampos
 ], login)
+
+router.post( '/google', [
+    check('token', 'El token de google es obligatorio').not().isEmpty(),
+    validarCampos
+], googleSignIn)
 
 
 
